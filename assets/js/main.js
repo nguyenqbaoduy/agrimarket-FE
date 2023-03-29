a650746b15f82c046f39812371aded5264598a3e
HuyHo2404 <hoquanghuy2404@gmail.com>
2023-03-30 00:04:18 +0700

    fix 29/03/2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,43 @@
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("toolbar-scroll").style.top = "0";
+//myheaders
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
   } else {
-    document.getElementById("toolbar-scroll").style.top = "-30px";
+    header.classList.remove("sticky");
   }
-  prevScrollpos = currentScrollPos;
 }
 
-//toggleMenu
+//toggleMenuAccount
 let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
   subMenu.classList.toggle("open-menu");
 }
+
+//toggleFavoriteProduct
+let btnlet = document.getElementById("btn-favorite");
+  function Toggle() {
+    if (btnlet.style.color == "var(--favorites-color)") {
+      btnlet.style.color = "var(--primary-color)";
+    }
+    else{
+      btnlet.style.color = "var(--favorites-color)"
+    }
+  }
+
+  let btnlet2 = document.getElementById("btn-favorite2");
+  function Toggle2() {
+    if (btnlet2.style.color == "var(--favorites-color)") {
+      btnlet2.style.color = "var(--primary-color)";
+    }
+    else{
+      btnlet2.style.color = "var(--favorites-color)"
+    }
+  }
+
+
