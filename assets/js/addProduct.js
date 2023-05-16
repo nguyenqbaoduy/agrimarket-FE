471456b026e3b48f2a2bf2bf6d575d41a60d7d33
HuynhDucHuy772001 <129087538+HuynhDucHuy772001@users.noreply.github.com>
2023-05-16 16:19:15 +0700

    News And Payment

--- assets/js/addProduct.js
+++ assets/js/addProduct.js
@@ -87,3 +87,19 @@ sidebar.addEventListener('mouseenter', function () {
 		})
 	}
 })
+
+let uploadButton = document.getElementById("upload-button");
+let choosenImage = document.getElementById("choosen-image");
+let fileName = document.getElementById("file-name");
+let imageUser = document.querySelector(".image-user");
+
+uploadButton.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadButton.files[0]);
+    console.log(uploadButton.files[0]);
+    reader.onload = () => {
+        choosenImage.setAttribute("src",reader.result);
+        imageUser.classList.add("active");
+    }
+    fileName.textContent = uploadButton.files[0].name;
+};
