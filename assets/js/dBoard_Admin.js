90dd8a8e6d9241bf685f941d517e8b654cb7ab35
HuynhDucHuy772001 <129087538+HuynhDucHuy772001@users.noreply.github.com>
2023-05-17 18:20:33 +0700

    Update Admin_ManageAccount_Dashboard

---
+++ assets/js/dBoard_Admin.js
@@ -0,0 +1,112 @@
+// SIDEBAR DROPDOWN
+const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
+const sidebar = document.getElementById('sidebar');
+
+allDropdown.forEach(item=> {
+	const a = item.parentElement.querySelector('a:first-child');
+	a.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		if(!this.classList.contains('active')) {
+			allDropdown.forEach(i=> {
+				const aLink = i.parentElement.querySelector('a:first-child');
+
+				aLink.classList.remove('active');
+				i.classList.remove('show');
+			})
+		}
+
+		this.classList.toggle('active');
+		item.classList.toggle('show');
+	})
+})
+
+//	PROFILE DROPDOWN
+const profile = document.querySelector('nav .profile');
+const  imgProfile = profile.querySelector('img');
+const dropdownProfile = profile.querySelector('.profile-link');
+
+imgProfile.addEventListener('click', function(){
+	dropdownProfile.classList.toggle('show')
+})
+
+window.addEventListener('click', function(e){
+	if(e.target !==imgProfile)	{
+		if(e.target !== dropdownProfile){
+			if(dropdownProfile.classList.contains('show')){
+				dropdownProfile.classList.remove('show');
+			}
+		}
+	}
+})
+
+// SIDEBAR COLLAPSE
+const allSideDivider = document.querySelectorAll('#sidebar .divider');
+
+if(sidebar.classList.contains('hide')) {
+	allSideDivider.forEach(item=> {
+		item.textContent = '-'
+	})
+	allDropdown.forEach(item=> {
+		const a = item.parentElement.querySelector('a:first-child');
+		a.classList.remove('active');
+		item.classList.remove('show');
+	})
+} else {
+	allSideDivider.forEach(item=> {
+		item.textContent = item.dataset.text;
+	})
+}
+
+
+sidebar.addEventListener('mouseleave', function () {
+	if(this.classList.contains('hide')) {
+		allDropdown.forEach(item=> {
+			const a = item.parentElement.querySelector('a:first-child');
+			a.classList.remove('active');
+			item.classList.remove('show');
+		})
+		allSideDivider.forEach(item=> {
+			item.textContent = '-'
+		})
+	}
+})
+
+
+
+sidebar.addEventListener('mouseenter', function () {
+	if(this.classList.contains('hide')) {
+		allDropdown.forEach(item=> {
+			const a = item.parentElement.querySelector('a:first-child');
+			a.classList.remove('active');
+			item.classList.remove('show');
+		})
+		allSideDivider.forEach(item=> {
+			item.textContent = item.dataset.text;
+		})
+	}
+})
+
+
+// Option
+const allOption = document.querySelectorAll('main .content-data .head .option');
+
+allOption.forEach(item=> {
+	const icon = item.querySelector('.icon');
+	const optionLink = item.querySelector('.option-link');
+
+	icon.addEventListener('click', function () {
+		optionLink.classList.toggle('show');
+	})
+})
+
+
+
+
+
+// PROGRESSBAR
+const allProgress = document.querySelectorAll('main .card .progress');
+
+allProgress.forEach(item=> {
+	item.style.setProperty('--value', item.dataset.value)
+})
