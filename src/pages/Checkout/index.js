0df8ee8c35193bd97a6e2c997c54d735752a4aef
abplocker <nguyenquangthien7100@gmail.com>
2023-05-21 03:08:53 +0700

    Update checkout

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,6 +1,7 @@
 import { } from 'react-router-dom';
 import React, { useEffect, useState } from "react";
 import { api_url, checkout } from "../../services/getAPI.js";
+import { getProvince, getDistrict, getWard, getTransports } from "../../services/getGHN.js";
 import { Link, useParams, useLocation } from "react-router-dom";
 import { useCookies } from 'react-cookie';
 import classNames from "classnames/bind";
@@ -9,49 +10,113 @@ const cx = classNames.bind(styles);
 const Checkout = () => {
     const location = useLocation();
     const items = location.state;
-    console.log(items)
+    const [provinces, setProvinces] = useState([]);
+    const [provincesId, setSelectedProvincesId] = useState();
+    const [districts, setDistricts] = useState([]);
+    const [districtsId, setSelectedDistrictsId] = useState();
+    const [wards, setWards] = useState([]);
+    const [wardsCode, setSelectedWardsId] = useState();
+    const [transports, setTransports] = useState([]);
+    const [transportsID, setSelectedTransportsId] = useState();
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const getProvinces = await getProvince();
+                setProvinces(getProvinces.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
         window.scrollTo(0, 0);
     }, []);
+
+    const handleOptionProvincesChange = async (e) => {
+        const selectedValue = e.target.value;
+        setSelectedProvincesId(selectedValue);
+        const getDistricts = await getDistrict(selectedValue);
+        setDistricts(getDistricts.data);
+    };
+    const handleOptionDistrictsChange = async (e) => {
+        const selectedValue = e.target.value;
+        setSelectedDistrictsId(selectedValue);
+        const getWards = await getWard(selectedValue);
+        setWards(getWards.data);
+    };
+    const handleOptionWardsChange = async (e) => {
+        const selectedValue = e.target.value;
+        setSelectedWardsId(selectedValue);
+        console.log(districtsId);
+        const getTransport = await getTransports(districtsId);
+        setTransports(getTransport.data);
+    };
+    const handleOptionTransportsChange = (e) => {
+        const selectedValue = e.target.value;
+        setSelectedTransportsId(selectedValue);
+    };
+
     return (
         <div className={cx('container')} id="">
             <div className={cx('item-flex')}>
                 {/* Checkout Section */}
                 <section className={cx('checkout')}>
-                    <h2 className={cx('section-headingpm')}>Payment Detail</h2>
+                    <h2 className={cx('section-headingpm')}>Thanh toán</h2>
                     <div className={cx('payment-form')}>
-                        <div className={cx('payment-method')}>
-                            <button className={cx('method', 'selected')}>
-                                <ion-icon name="card"></ion-icon>
-                                <span>Credit Card</span>
-                                <ion-icon className={cx('checkmark', 'fill')} name="checkmark-circle"></ion-icon>
-                            </button>
-                            <button className={cx('method')}>
-                                <ion-icon name="logo-paypal"></ion-icon>
-                                <span>Paypal</span>
-                                <ion-icon className={cx('checkmark')} name="checkmark-circle-outline"></ion-icon>
-                            </button>
+                        <label htmlFor="cardholder-name" className={cx('label-default')}>Địa chỉ nhận hàng</label>
+                        <div className={cx('address')}>
+                            <div className={cx('method')}>
+                                <select value={provincesId} onChange={handleOptionProvincesChange}>
+                                    <option value="">-- Tỉnh/Thành phố --</option>
+                                    {provinces && provinces.map(province => (
+                                        <option key={province.ProvinceID} value={province.ProvinceID}>
+                                            {province.ProvinceName}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className={cx('method')}>
+                                <select value={districtsId} onChange={handleOptionDistrictsChange}>
+                                    <option value="">-- Quận/Huyện --</option>
+                                    {districts && districts.map(district => (
+                                        <option key={district.DistrictID} value={district.DistrictID}>
+                                            {district.DistrictName}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className={cx('method')}>
+                                <select value={wardsCode} onChange={handleOptionWardsChange}>
+                                    <option value="">-- Phường/Xã--</option>
+                                    {wards && wards.map(ward => (
+                                        <option key={ward.WardCode} value={ward.WardCode}>
+                                            {ward.WardName}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
                         </div>
                         <form action="#">
                             <div className={cx('cardholder-name')}>
-                                <label htmlFor="cardholder-name" className={cx('label-default')}>Cardholder name</label>
+                                <label htmlFor="cardholder-name" className={cx('label-default')}>Địa chỉ cụ thể</label>
                                 <input type="text" name="cardholder-name" id="cardholder-name" className={cx('input-default')} />
                             </div>
                             <div className={cx('card-number')}>
-                                <label htmlFor="card-number" className={cx('label-default')}>Card number</label>
+                                <label htmlFor="card-number" className={cx('label-default')}>Số điện thoại</label>
                                 <input type="text" name="card-number" id="card-number" className={cx('input-default')} />
                             </div>
                             <div className={cx('input-flex')}>
-                                <div className={cx('expire-date')}>
-                                    <label htmlFor="expire-date" className={cx('label-default')}>Expiration date</label>
-                                    <div className={cx('input-flex')}>
-                                        <input type="number" name="day" id="expire-date" placeholder="31" min="1" max="31" className={cx('input-default')} />
-                                        <input type="number" name="month" id="expire-date" placeholder="12" min="1" max="12" className={cx('input-default')} />
+                                <div className={cx('transport')}>
+                                    <label htmlFor="ccv" className={cx('label-default')}>Phương thức vận chuyển</label>
+                                    <div className={cx('method')}>
+                                        <select value={transportsID} onChange={handleOptionTransportsChange}>
+                                            <option value="">-- Vận chuyển--</option>
+                                            {transports && transports.map(transport => (
+                                                <option key={transport.service_type_id} value={transport.service_type_id}>
+                                                    {transport.short_name}
+                                                </option>
+                                            ))}
+                                        </select>
                                     </div>
-                                </div>
-                                <div className={cx('cvv')}>
-                                    <label htmlFor="ccv" className={cx('label-default')}>CVV</label>
-                                    <input type="text" name="ccv" id="ccv" className={cx('input-default')} />
                                 </div>
                             </div>
                         </form>
@@ -68,7 +133,7 @@ const Checkout = () => {
                             <div className={cx('product-card')}>
                                 <div className={cx('card')}>
                                     <div className={cx('img-box')}>
-                                        <img src={api_url + "/images/product/"+item.ProductImageDefault} alt="" className={cx('product-img')} />
+                                        <img src={api_url + "/images/product/" + item.ProductImageDefault} alt="" className={cx('product-img')} />
                                     </div>
                                     <div className={cx('detail')}>
                                         <h4 className={cx('product-name')}>{item.ProductName}</h4>
---
+++ src/services/getGHN.js
@@ -0,0 +1,54 @@
+const token = "c7a1b596-f72f-11ed-82fc-92443ce24152";
+const province = "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province"
+const district = "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/district?province_id="
+const ward = "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/ward?district_id="
+const transport = "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services"
+export async function getProvince() {
+    const response = await fetch(province, {
+        method: "GET",
+        headers: {
+            'token': token
+        },
+    });
+    return response.json();
+}
+export async function getDistrict(provinceID) {
+    console.log("getDistrict");
+    const response = await fetch(district+provinceID, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            'token': token
+        },
+    });
+    const data = response.json()
+    return data;
+}
+export async function getWard(districtID) {
+    const response = await fetch(ward+districtID, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            'token': token
+        },
+    });
+    return response.json();
+}
+
+export async function getTransports(to_districtsId) {
+    const data = {
+        'shop_id': 124245,
+        'from_district': 1530, //Liên Chiểu, Đà Nẵng
+        'to_district' : parseInt(to_districtsId)
+    }
+    console.log(data)
+    const response = await fetch(transport, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'token': token
+        },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
