b8d366a82a5dfcb0cdeb6597394eab83156b4b17
abplocker <nguyenquangthien7100@gmail.com>
2023-05-24 11:20:41 +0700

    Fix

--- src/App.js
+++ src/App.js
@@ -4,23 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { publicPath } from "./routes";
 import { Fragment } from "react";
 import "bootstrap/scss/bootstrap.scss";
-import { useCookies } from "react-cookie";
-import { authentication } from "./services/getAPI";
 
 function App() {
-  const [cookies, setCookie] = useCookies([]);
-  const [hasSetCookie, setHasSetCookie] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      setHasSetCookie(false);
-      var ath = await authentication(cookies.accessToken, cookies.refreshToken);
-      if (!hasSetCookie) {
-        setCookie('accessToken', ath.accessToken, { path: "/" });
-        setHasSetCookie(true);
-      }
-    };
-    fetchData();
-  },[cookies.accessToken, cookies.refreshToken, hasSetCookie, setCookie]);
   return (
     <Router>
       <div className="App">
